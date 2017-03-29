12d4caa9e9878fbec362ab4ef9f0bced049ad47b
ejoysutton <joy.ericka.sutton@gmail.com>
2017-03-28 23:16:50 -0400

    More styling

---
+++ public/javascripts/script.js
@@ -0,0 +1,8 @@
+// $( document ).ready(function() {
+// 	// var currentUser = req && req.session && req.session.currentUser;
+//     if ($(currentUser)) {
+//       $('#loginForm').setAttribute('hidden');
+//     } else {
+
+//     }
+// });
